cb9f10ea5159eebad67170ccd594cbc1fb381280
17NinosO <kodiomume@gmail.com>
2026-07-12 18:07:50 +0100

    Add velocity-inlet/pressure-outlet/bounce-back-wall boundary conditions

--- IRP/PorousChannel/porous_channel.cpp
+++ IRP/PorousChannel/porous_channel.cpp
@@ -11,14 +11,40 @@ typedef double T;
 #define DESCRIPTOR descriptors::D2Q9Descriptor
 
 namespace param {
-    const plint Lx = 200;   // placeholder domain size -- Step 2c will size
-    const plint Ly = 100;   // this from the actual pore_geometry mask instead
-    const T tau = 0.8;      // placeholder -- Step 3 derives this from Re
+    const plint Lx = 200;
+    const plint Ly = 100;
+    const T tau = 0.8;
     const T omega = 1.0 / tau;
-    const T U_lb = 0.02;    // small, safe placeholder inlet-ish velocity
+    const T U_lb = 0.02;
     const plint maxIter = 500;
     const plint outIter = 100;
 }
+
+// -------------------- NEW: goes here, above main() --------------------
+void setBoundaryConditions(MultiBlockLattice2D<T, DESCRIPTOR>& lattice) {
+    using namespace param;
+    Array<T, 2> u_inf(U_lb, 0.0);
+
+    OnLatticeBoundaryCondition2D<T, DESCRIPTOR>* bc =
+        createLocalBoundaryCondition2D<T, DESCRIPTOR>();
+
+    bc->addVelocityBoundary0N(Box2D(0, 0, 1, Ly - 2), lattice);
+    setBoundaryVelocity(lattice, Box2D(0, 0, 1, Ly - 2), u_inf);
+
+    bc->addPressureBoundary0P(Box2D(Lx - 1, Lx - 1, 1, Ly - 2), lattice);
+    setBoundaryDensity(lattice, Box2D(Lx - 1, Lx - 1, 1, Ly - 2), (T)1.0);
+
+    defineDynamics(lattice, Box2D(1, Lx - 2, 0, 0), new BounceBack<T, DESCRIPTOR>());
+    defineDynamics(lattice, Box2D(1, Lx - 2, Ly - 1, Ly - 1), new BounceBack<T, DESCRIPTOR>());
+
+    defineDynamics(lattice, Box2D(0, 0, 0, 0), new BounceBack<T, DESCRIPTOR>());
+    defineDynamics(lattice, Box2D(0, 0, Ly - 1, Ly - 1), new BounceBack<T, DESCRIPTOR>());
+    defineDynamics(lattice, Box2D(Lx - 1, Lx - 1, 0, 0), new BounceBack<T, DESCRIPTOR>());
+    defineDynamics(lattice, Box2D(Lx - 1, Lx - 1, Ly - 1, Ly - 1), new BounceBack<T, DESCRIPTOR>());
+
+    delete bc;
+}
+// ------------------------------------------------------------------------
 
 int main(int argc, char* argv[]) {
     plbInit(&argc, &argv);
@@ -32,6 +58,8 @@ int main(int argc, char* argv[]) {
     MultiBlockLattice2D<T, DESCRIPTOR> lattice(
         param::Lx, param::Ly,
         new BGKdynamics<T, DESCRIPTOR>(param::omega));
+
+    setBoundaryConditions(lattice);   // <-- NEW: this one line, right here
 
     Array<T, 2> u0(param::U_lb, 0.0);
     initializeAtEquilibrium(lattice, lattice.getBoundingBox(), (T)1.0, u0);
